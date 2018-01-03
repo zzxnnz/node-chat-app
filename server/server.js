736d404336aa0bd6490a58c5ba85582a393f830e
Ruslan J <leonemsolis@gmail.com>
2018-01-03 15:40:18 +0900

    Init the project

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname, "/../public");
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+// if(!module.parent) {
+//     app.listen(port, () => {
+//         console.log("Started at port", port);
+//     });
+// }
+
+app.listen(port, () => {
+    console.log(`Started at port ${port}`);
+});
+
+module.exports = {app};
+
